09196f5bed7336f49e4dfa2ac9cdbca2f28875b1
MaximKemmel <52887742+MaximKemmel@users.noreply.github.com>
2024-06-18 15:08:38 +0300

    GamesList

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./App.module.sass";
@@ -12,11 +12,25 @@ import InfoIcon from "./assets/information.png";
 const telegram = window.Telegram.WebApp;
 
 function App() {
+  const [selectedGame, setSelectedGame] = useState(-1);
+
   useEffect(() => {
     telegram.ready();
     telegram.expand();
     telegram.setHeaderColor("#2C2D3F");
   }, []);
+
+  useEffect(() => {
+    if (selectedGame === -1) {
+      if (telegram.BackButton.isVisible) {
+        telegram.BackButton.hide();
+      }
+    } else {
+      if (!telegram.BackButton.isVisible) {
+        telegram.BackButton.show();
+      }
+    }
+  }, [selectedGame]);
 
   return (
     <div className={styles.main}>
@@ -26,7 +40,7 @@ function App() {
             <img src={game.icon} alt="" />
             <div className={styles.name}>{game.name}</div>
             <div className={styles.buttons}>
-              <button>
+              <button onClick={() => setSelectedGame(game.id)}>
                 <img src={InfoIcon} alt="" />
               </button>
               <Link to={game.url}>
