24a9bb1d3730826fce1b366a3d91dbdabcd5e02d
MaximKemmel <52887742+MaximKemmel@users.noreply.github.com>
2024-06-21 00:12:46 +0300

    HamsterKombat

--- src/pages/HamsterKombat/HamsterKombat.tsx
+++ src/pages/HamsterKombat/HamsterKombat.tsx
@@ -9,6 +9,10 @@ import { Joystick as JoystickIcon } from "../../assets/svg/Joystick";
 import { Search as SearchIcon } from "../../assets/svg/Search";
 import { Wallet as WalletIcon } from "../../assets/svg/Wallet";
 
+import GameFiIcon from "../../assets/hamster_kombat/gamefi_tokens.svg";
+import P2PTradingIcon from "../../assets/hamster_kombat/p2p_trading.png";
+import VillaForTheDEVTeamIcon from "../../assets/hamster_kombat/villa_for_dev_team.png";
+
 const HamsterKombat = () => {
   const [selectedTab, setSelectedTab] = useState(0);
 
@@ -36,24 +40,59 @@ const HamsterKombat = () => {
         </div>
         <div className={styles.tabs_contents}>
           <div className={`${styles.content} ${selectedTab === 0 ? styles.active : ""}`}>5445</div>
-          <div className={`${styles.content} ${selectedTab === 1 ? styles.active : ""}`}>654646</div>
+          <div className={`${styles.content} ${selectedTab === 1 ? styles.active : ""}`}>
+            <div className={styles.row}>
+              <SearchIcon />
+              Комбо-карточки (20-21 июня):
+            </div>
+            <div className={`${styles.row} ${styles.combo}`}>
+              <div className={styles.card}>
+                <img src={GameFiIcon} alt="" />
+                <div className={styles.name}>GameFi tokens</div>
+              </div>
+              <div className={styles.card}>
+                <img src={P2PTradingIcon} alt="" />
+                <div className={styles.name}>P2P trading</div>
+              </div>
+              <div className={styles.card}>
+                <img src={VillaForTheDEVTeamIcon} alt="" />
+                <div className={styles.name}>Villa for the DEV team</div>
+              </div>
+            </div>
+            <div className={`${styles.row} ${styles.vertical}`}>
+              Где найти карточки:
+              <div className={`${styles.row} ${styles.combo_description}`}>
+                <span>GameFi tokens</span> - Markets
+              </div>
+              <div className={`${styles.row} ${styles.combo_description}`}>
+                <span>P2P trading</span> - Markets
+              </div>
+              <div className={`${styles.row} ${styles.combo_description}`}>
+                <span>Villa for the DEV team</span> - Specials
+              </div>
+            </div>
+            <div className={styles.row}>
+              <WalletIcon />
+              Получаемая награда = <span>5 000 000</span>
+            </div>
+          </div>
           <div className={`${styles.content} ${selectedTab === 2 ? styles.active : ""}`}>
             <div className={styles.row}>
               <SearchIcon />
-              Новый шифр = <span>FORK</span>
+              Шифр (20-21 июня) = <span>PUMP</span>
             </div>
             <div className={`${styles.row} ${styles.code}`}>
               <div className={styles.code_letter}>
-                <div className={styles.position}>1</div>F<span>••-•</span>
+                <div className={styles.position}>1</div>P<span>•--•</span>
               </div>
               <div className={styles.code_letter}>
-                <div className={styles.position}>2</div>O<span>---</span>
+                <div className={styles.position}>2</div>U<span>••-</span>
               </div>
               <div className={styles.code_letter}>
-                <div className={styles.position}>3</div>R<span>•-•</span>
+                <div className={styles.position}>3</div>M<span>--</span>
               </div>
               <div className={styles.code_letter}>
-                <div className={styles.position}>4</div>K<span>-•-</span>
+                <div className={styles.position}>4</div>P<span>•--•</span>
               </div>
             </div>
             <div className={`${styles.row} ${styles.vertical}`}>
@@ -66,7 +105,6 @@ const HamsterKombat = () => {
                 <span>-</span>(тире) - это долгий тап (~2 сек)
               </div>
             </div>
-
             <div className={styles.row}>
               <WalletIcon />
               Получаемая награда = <span>1 000 000</span>
