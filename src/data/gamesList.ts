bd83c3f4b8e463f9df6f8777ce9ff90437324af3
MaximKemmel <52887742+MaximKemmel@users.noreply.github.com>
2024-06-22 20:12:39 +0300

    GamesList

--- src/data/gamesList.ts
+++ src/data/gamesList.ts
@@ -12,6 +12,17 @@ import MemeFiIcon from "../assets/meme_fi.jpg";
 import PocketFiIcon from "../assets/pocket_fi.jpg";
 import FuelJettonIcon from "../assets/fuel_jetton.jpg";
 import BodyPumpIcon from "../assets/body_pump.jpg";
+import NotcoinIcon from "../assets/notcoin.jpg";
+import BullRunIcon from "../assets/bull_run.jpg";
+import CubesIcon from "../assets/cubes.jpg";
+import YesCoinIcon from "../assets/yes_coin.jpg";
+import ChainGameIcon from "../assets/chain_game.jpg";
+import HarvestMoonIcon from "../assets/harvest_moon.jpg";
+import BBQCoinIcon from "../assets/bbq_coin.jpg";
+import PixelTapIcon from "../assets/pixel_tap.jpg";
+import TapSwapIcon from "../assets/tap_swap.jpg";
+import PocketRocketIcon from "../assets/pocket_rocket.jpg";
+import DotcoinIcon from "../assets/dotcoin.jpg";
 
 export const GameList = [
   {
@@ -97,6 +108,83 @@ export const GameList = [
     name: "Body Pump",
     url: "https://t.me/Bodypump_app_bot?start=4c6135d3-5dd6-433b-87be-09143d14d430",
     icon: BodyPumpIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 12,
+    name: "Notcoin 2.0",
+    url: "https://t.me/notcoin_bot",
+    icon: NotcoinIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 13,
+    name: "Bull Run",
+    url: "https://t.me/BullApp_bot?start=406006297_46",
+    icon: BullRunIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 14,
+    name: "Cubes",
+    url: "https://t.me/cubesonthewater_bot?start=NDA2MDA2Mjk3",
+    icon: CubesIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 15,
+    name: "YesCoin",
+    url: "https://t.me/YesCoin_ebot?start=406006297",
+    icon: YesCoinIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 16,
+    name: "Chain Game",
+    url: "https://t.me/onchaincoin_bot",
+    icon: ChainGameIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 17,
+    name: "Harvest Moon",
+    url: "https://harvestmoon.meteorwallet.app/nearharvestmoonbot?start=NDA2MDA2Mjk3OmtlbWRlY28=",
+    icon: HarvestMoonIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 18,
+    name: "BBQ Coin",
+    url: "https://t.me/BBQcoin_bot/BBQcoin?startapp=rp_9001869_406006297",
+    icon: BBQCoinIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 19,
+    name: "PixelTap",
+    url: "https://t.me/pixelversexyzbot?start=406006297",
+    icon: PixelTapIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 20,
+    name: "TapSwap",
+    url: "https://t.me/tapswap_mirror_bot?start=r_406006297",
+    icon: TapSwapIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 21,
+    name: "PocketRocket",
+    url: "https://t.me/pocket_rocket_game_bot?start=e4acbb4e-0a39-44ec-ab08-afb1408516ac",
+    icon: PocketRocketIcon,
+    show_description: false,
+  } as IGame,
+  {
+    id: 22,
+    name: "Dotcoin",
+    url: "https://t.me/dotcoin_bot?start=r_406006297",
+    icon: DotcoinIcon,
     show_description: false,
   } as IGame,
 ] as IGame[];
